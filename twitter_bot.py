3b6cddb9bc96ec4a9cad5791ef6d2b29d9f52ff8
Karolin Flach <karolinflach@gmail.com>
2019-06-25 06:58:47 +0200

    modified twitter_bot

--- twitter_bot.py
+++ twitter_bot.py
@@ -1,8 +1,10 @@
 import tweepy
-import os
 import configparser
+import os
 
-def get_tokens():
+
+def get_twitter_tokens():
+
     config = configparser.ConfigParser()
     config.read('config.ini')
     consumer_key = config['TWITTER']['consumer_key']
@@ -12,21 +14,55 @@ def get_tokens():
     return consumer_key, consumer_secret, access_token, access_token_secret
 
 
-consumer_key, consumer_secret, access_token, access_token_secret = get_tokens()
+def get_filename_for_tweet():
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    # selects the first image from sorted list of files in current directory
 
-api = tweepy.API(auth)
+    directory = os.getcwd()
+    my_dir = os.listdir(directory)
+    all_files = sorted([file for file in my_dir if file.endswith(".jpg")])
+    filename = all_files[0]
 
-api.update_status(status="I loove pasta!")
+    return filename
 
-# os.chdir("/Users/karolin/PycharmProjects/my_project/Pastabot/pastapics/")
-#
-# file = open('pastapics-2.jpg', 'rb')
-# r1 = api.media_upload(filename='pastapics-2.jpg', file=file)
-# print(r1)
-# print(r1.media_id_string)
-# media_ids = [r1.media_id_string]
-# print(media_ids)
-# api.update_status(media_ids=media_ids, status="<3")
+
+def remove_tweeted_image():
+
+    # removes the image after it's been tweeted
+
+    delete_file = get_filename_for_tweet()
+    os.remove(delete_file)
+
+
+def get_twitter_media_id(filename):
+
+    file = open(filename, 'rb')
+    upload = api.media_upload(filename=filename, file=file)
+    media_id = [upload.media_id_string]
+
+    return media_id
+
+
+if __name__ == '__main__':
+
+    consumer_key, consumer_secret, access_token, access_token_secret = get_twitter_tokens()
+
+    print('trying to connect...')
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    print('Connected!\n')
+
+    os.chdir('pastapics')
+
+    file_to_tweet = get_filename_for_tweet()
+    media_id = get_twitter_media_id(file_to_tweet)
+
+    api.update_status(media_ids=media_id)
+
+    remove_tweeted_image()
+
+
+
